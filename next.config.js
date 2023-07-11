d18b5c13e01e02e88389915f1cafbf8de61662d9
Jeff Huang <jeff@spatial.io>
2023-07-11 16:35:42 -0400

    repro: next/image ttl bug when deployed to Vercel

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import("next").NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  images: {
+    // 10 minutes
+    minimumCacheTTL: 600,
+    remotePatterns: [{ protocol: "https", hostname: "renderapi.s3.amazonaws.com" }],
+  },
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,10 @@
+import Image from "next/image"
 /** Add your relevant code here for the issue to reproduce */
 export default function Home() {
-  return null
+  return (
+    <div>
+      {/* remote image that does not set Cache-Control header */}
+      <Image src="https://renderapi.s3.amazonaws.com/D5DH7jDhj.png" alt="" width="96" height="96" />
+    </div>
+  )
 }
